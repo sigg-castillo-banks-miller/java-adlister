59f1c9ea28ef3fd537d21aa677b25414cce3ae4c
Michael J Sigg <mjsigg@gmail.com>
2022-07-25 16:57:00 -0500

    end of day

--- src/main/java/com/codeup/adlister/dao/ListAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/ListAdsDao.java
@@ -32,6 +32,11 @@ public class ListAdsDao implements Ads {
         return null;
     }
 
+    @Override
+    public Long delete(Ad ad) {
+        return null;
+    }
+
     private List<Ad> generateAds() {
         List<Ad> ads = new ArrayList<>();
         ads.add(new Ad(
